5b0c8392aa1340f8e4038b0339dd59cf663d570c
Chris <cccardone@softstrategy.it>
2024-09-29 12:38:50 +0200

    Added Loader and handled server hydration error next

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -3,28 +3,38 @@
 import { Canvas } from "@react-three/fiber";
 import { SodaCan } from "./SodaCan";
 import { Environment, Float, View } from "@react-three/drei";
+import { Suspense } from "react";
+import dynamic from 'next/dynamic'
+
 type Props = {};
+
+const Loader = dynamic(() => import('@react-three/drei').then((mod) => mod.Loader), { ssr: false })
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-      camera={{
-        fov: 30,
-      }}
-    >
-        <View.Port />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{
+          fov: 30,
+        }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
